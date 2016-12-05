79042551cc94ed88fe3e7ea1fd10e43e9de67b5b
sgrandner <stefan.grandner@stayfriends.com>
2016-12-05 14:17:28 +0100

    Some changes for persistence unit, does not work

--- src/main/java/com/blaeser/services/PageService.java
+++ src/main/java/com/blaeser/services/PageService.java
@@ -5,7 +5,11 @@ import com.blaeser.models.Page;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import javax.persistence.TypedQuery;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 
 @Stateless
@@ -16,6 +20,29 @@ public class PageService {
 
 	public List<Page> getAll() {
 
-		return null;
+		Query query = entityManager.createQuery(
+				"SELECT p.id, p.name" +
+				"FROM pages AS p" +
+				"WHERE p.id > 1");
+
+//		query.setParameter('1', "1");
+
+		List<?> tempResultList = query.getResultList();
+		Iterator<?> iterator = tempResultList.iterator();
+
+		List<Page> resultList = new ArrayList<>();
+
+		while (iterator.hasNext()) {
+
+			Object[] row = (Object[]) iterator.next();
+
+			Page tempPage = new Page();
+			tempPage.setId((Integer) row[0]);
+			tempPage.setName((String) row[1]);
+
+			resultList.add(tempPage);
+		}
+
+		return resultList;
 	}
 }
